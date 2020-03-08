8f9cccc036eea7c8ce54eac5f617a388221ea48a
Sudarshan <31204774+sudface@users.noreply.github.com>
2020-03-08 19:21:26 +1100

    Add files via upload

---
+++ 8 - Optimizing Code 2/code.vb
@@ -0,0 +1,6 @@
+﻿Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click, Button10.Click, Button11.Click, Button12.Click, Button13.Click, Button14.Click, Button15.Click, Button16.Click, Button17.Click, Button18.Click, Button19.Click, Button20.Click, Button21.Click, Button22.Click, Button23.Click, Button24.Click, Button25.Click, Button26.Click, Button27.Click, Button28.Click, Button29.Click, Button30.Click, Button31.Click, Button32.Click, Button33.Click, Button34.Click, Button35.Click, Button36.Click, Button37.Click, Button38.Click, Button39.Click, Button40.Click
+	Dim btn As Button = DirectCast(sender, Button)
+	btn.Text = "Hello"
+	btn.Enabled = False
+End Sub
+
